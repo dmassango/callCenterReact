50682252fc5d29cf225ec0c0f4cbbb7416b22bd4
dario massango <dariodimas.massango@cedsif.gov.mz>
2025-09-24 11:41:22 +0200

    Aula 122

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -124,7 +124,9 @@ function AuthProvider({ children }){
         signUp,
         logout,
         loadingAuth,
-        loading
+        loading,
+        setUser,
+        storageUser
       }}
     >
       {children}
